0c95d65373a8e902bfb8aa778102f2b171a0cdf7
kiragom <wojin1212@gmail.com>
2022-09-15 22:00:36 +0900

    buddy system with simple test

---
+++ buddy.h
@@ -0,0 +1,45 @@
+//#define HASHSIZE    32771
+#define HASHSIZE    3
+
+struct FREELIST {
+    struct FREELIST *next;
+};
+
+struct ORDERLIST {
+    struct FREELIST *freeList;
+};
+
+struct NODE {
+    void *key;
+    int value;
+    struct NODE *next;
+    struct NODELIST *list;
+};
+
+struct NODELIST {
+    struct NODE *address;
+    struct NODELIST *next;
+};
+
+struct HASHTABLE {
+    struct NODE *head;
+};
+struct HASHTABLE bucket[HASHSIZE];
+
+extern char end[];
+
+void buddyInit(void *vstart, void *vend);
+char *dmalloc(int size);
+int calOrder(int size);
+char *divideOrder(int targetOrder, int allocOrder);
+void printBuddySystem();
+
+void linkNodeList();
+struct NODE *createNode(void *key, int value);
+int hashFunc(void *key);
+void addSize(void *start, int size);
+int removeSize(void *start);
+void printHash();
+
+void dfree(char *address);
+void mergeOrder(int targetOrder);
