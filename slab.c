6874f145bdebc67d905bc1de20fea2bb35f0de10
Elquinex <nicepark268@naver.com>
2022-09-19 00:37:09 +0900

    hash table

--- slab.c
+++ slab.c
@@ -5,16 +5,8 @@
 #include "memlayout.h"
 #include "mmu.h"
 
-struct {
-  uint obj_sz;
-  uint free_page_num;
-
-  struct page static_page;
-
-  struct page* cur_slab_page;
-  struct page* page_tail;
-
-} slab[SLAB_NUM];
+struct slab_cache slab[SLAB_NUM];
+struct hash_ptr hash_table[HASH_SIZE];
 
 void make_slab_obj(struct page* cur_page, uint obj_sz){
   uint cnt = 1;
@@ -45,33 +37,38 @@ struct page alloc_new_slab_page(uint slab_idx){
   return new_page;
 }
 
-struct page_free find_target_page(uint page_addr){
-  struct page_free ret_val;
+struct hash_node* find_target_page(uint hash_idx, uint page_addr){
+  struct hash_node* cur_node = hash_table[hash_idx].head;
 
-  memset(&ret_val, 0, sizeof(ret_val));
-  for(uint i=0; i<SLAB_NUM; i++){
-    struct page* cur_page = slab[i].cur_slab_page;
-    //if(i==0) cprintf("cur_page : %x page_addr : %x\n", cur_page->page_start, page_addr);
-    while(cur_page){
-      if(((uint)(cur_page->page_start)) == page_addr) {
-        ret_val.cur_page = cur_page;
-        ret_val.slab_idx = i;
-        return ret_val;
-      }
-      cur_page = cur_page->next;
-    }
+  while(cur_node){
+    if(((uint)cur_node->cur_page->page_start) == page_addr) return cur_node;
+    cur_node = cur_node->next;
   }
 
-  return ret_val;
+  return 0;
+}
+
+uint check_slab_obj(uint obj_addr, uint obj_sz){
+  obj_addr %= PGSIZE;
+
+  if(obj_addr % obj_sz != 0) return 0;
+  else return 1;
 }
 
 void free_slab_obj(void* slab_obj){
-  struct page_free tpage = find_target_page((((uint)slab_obj) / PGSIZE) * PGSIZE);
+  uint page_addr = (((uint)slab_obj) / PGSIZE) * PGSIZE;
+  uint hash_idx = calc_hash_idx(page_addr);
 
-  struct page* tar_page = tpage.cur_page;
-  uint slab_idx = tpage.slab_idx;
+  struct hash_node* hnode = find_target_page(hash_idx, page_addr);
 
-  //cprintf("slab_idx : %x tar_page : %x\n", slab_idx, tar_page);
+  if(hnode == 0) return;
+
+  struct page* tar_page = hnode->cur_page;
+  uint slab_idx = hnode->slab_idx;
+
+  if(!check_slab_obj((uint)slab_obj, slab[slab_idx].obj_sz)) return;
+
+  //cprintf("slab_idx : %d hnode : %x\n", slab_idx, hnode);
 
   if(tar_page == 0) return;
 
@@ -82,6 +79,7 @@ void free_slab_obj(void* slab_obj){
 
     if(slab[slab_idx].free_page_num > MAX_FREE_PAGE){
       return_slab_page(slab_idx, tar_page);
+      return_hash_node(hash_idx, hnode);
     }
   }
 }
@@ -89,6 +87,48 @@ void free_slab_obj(void* slab_obj){
 void free_slab_page(uint slab_idx, struct page* slab_page){
   kfree(slab_page->page_start);
   free_slab_obj(slab_page);
+}
+
+uint calc_hash_idx(uint page_addr){
+  return (page_addr / PGSIZE) % HASH_SIZE;
+}
+
+void insert_hash_node(uint slab_idx, struct page* slab_page){
+  uint hash_idx = calc_hash_idx((uint)(slab_page->page_start));
+
+  struct hash_node* cur_node = (struct hash_node*)kmalloc(sizeof(struct hash_node));
+  cur_node->cur_page = slab_page;
+  cur_node->slab_idx = slab_idx;
+
+  if(hash_table[hash_idx].tail == 0){
+    hash_table[hash_idx].head = cur_node;
+    hash_table[hash_idx].tail = cur_node;
+    cur_node->prev = cur_node->next = 0;
+  }
+  else{
+    hash_table[hash_idx].tail->next = cur_node;
+    cur_node->next = 0;
+    cur_node->prev = hash_table[hash_idx].tail;
+    hash_table[hash_idx].tail = cur_node;
+  }
+}
+
+void return_hash_node(uint hash_idx, struct hash_node* tar_node){
+  if(tar_node->prev != 0){
+    tar_node->prev->next = tar_node->next;
+  }
+  if(tar_node->next != 0){
+    tar_node->next->prev = tar_node->prev;
+  }
+
+  if(hash_table[hash_idx].head == tar_node){
+    hash_table[hash_idx].head = tar_node->next;
+  }
+  if(hash_table[hash_idx].tail == tar_node){
+    hash_table[hash_idx].tail = tar_node->prev;
+  }
+
+  free_slab_obj(tar_node);
 }
 
 void insert_slab_page(uint flag, uint slab_idx, struct page* slab_page){
@@ -172,6 +212,7 @@ void* pop_free_list(uint slab_idx){
 void slab_init(void){
   uint slab_idx = 0;
 
+  memset(hash_table, 0, sizeof(hash_table));
   for(uint sz = 4; sz <= 2048; sz += sz, slab_idx++){
     slab[slab_idx].obj_sz = sz;
     slab[slab_idx].cur_slab_page = 0;
@@ -179,6 +220,10 @@ void slab_init(void){
     slab[slab_idx].static_page = alloc_new_slab_page(slab_idx);
     insert_slab_page(TAIL, slab_idx, &(slab[slab_idx].static_page));
     slab[slab_idx].cur_slab_page = &(slab[slab_idx].static_page);
+  }
+
+  for(uint slab_idx = 0; slab_idx < SLAB_NUM; slab_idx++){
+    insert_hash_node(slab_idx, slab[slab_idx].cur_slab_page);
   }
 }
 
@@ -196,6 +241,22 @@ void print_slab(uint slab_idx){
   }
 
   cprintf("-----------------------\n");
+}
+
+void print_hash(void){
+  cprintf("\n-------hash table--------\n");
+  for(uint hash_idx = 0; hash_idx < HASH_SIZE; hash_idx++){
+    cprintf("hash idx[%d] : ", hash_idx);
+    struct hash_node* cur_node = hash_table[hash_idx].head;
+
+    while(cur_node){
+      cprintf("%x -> ", cur_node->cur_page);
+      cur_node = cur_node->next;
+    }
+
+    cprintf("\n");
+  }
+  cprintf("--------------------------\n");
 }
 
 uint calc_slab_idx(uint N){
@@ -238,7 +299,7 @@ page_try_alloc:
 page_no_free_obj:
   if((cur_page->next != 0) && (cur_page != prev_tail)){
     load_next_slab_page(slab_idx);
-    cprintf("load next page obj page : %x\n", slab[slab_idx].cur_slab_page);
+    //cprintf("load next page obj page : %x\n", slab[slab_idx].cur_slab_page);
     goto page_try_alloc;
   }
   else{
@@ -253,8 +314,9 @@ page_no_free_obj:
 
     *new_page_addr = new_page;
     insert_slab_page(HEAD, slab_idx, new_page_addr);
+    insert_hash_node(slab_idx, new_page_addr);
 
-    cprintf("alloc page obj page: %x\n", new_page_addr);
+    //cprintf("alloc page obj page: %x\n", new_page_addr);
     goto page_try_alloc;
   }
 
@@ -274,15 +336,14 @@ try_alloc:
     return pop_free_list(slab_idx);
   }
   else{
-    cprintf("\n-------slab_system call------\n");
+    //cprintf("\n-------slab_system call------\n");
     goto no_free_obj;
   }
 
 no_free_obj:
   if((cur_page->next != 0) && (cur_page != prev_tail)){
-    cprintf("prev_tail : %x\n", prev_tail);
     load_next_slab_page(slab_idx);
-    cprintf("load next slab page : %x\n", slab[slab_idx].cur_slab_page);
+    //cprintf("load next slab page : %x\n", slab[slab_idx].cur_slab_page);
     goto try_alloc;
   }
   else{
@@ -297,8 +358,9 @@ no_free_obj:
       *new_page_addr = new_page;
       
       insert_slab_page(HEAD, slab_idx, new_page_addr);
+      insert_hash_node(slab_idx, new_page_addr);
 
-      cprintf("alloc new slab page : %x\n", slab[slab_idx].cur_slab_page);
+      //cprintf("alloc new slab page : %x\n", slab[slab_idx].cur_slab_page);
       goto try_alloc;
     }
     else{
@@ -317,7 +379,7 @@ void* kmalloc(uint N){
   return slab_system(N);
 }
 
-typedef uint TEST_TYPE;
+typedef struct hash_node TEST_TYPE;
 TEST_TYPE* test_mem[(PGSIZE / sizeof(TEST_TYPE)) * 10];
 
 void slab_test(void){
@@ -327,6 +389,7 @@ void slab_test(void){
     uint test_unit = (PGSIZE / slab[slab_idx].obj_sz);
 
     print_slab(slab_idx);
+    print_hash();
 
     for(uint i=0; i < (3 * test_unit); i++){
         TEST_TYPE* testptr = (TEST_TYPE*)kmalloc(sizeof(TEST_TYPE));
@@ -337,10 +400,10 @@ void slab_test(void){
         }
 
         test_mem[i] = testptr;
-        *testptr = i % PGSIZE;
+        testptr->slab_idx = i % PGSIZE;
 
-        if(*testptr != (i % PGSIZE)) {
-            cprintf("assert : write %x & read %x\n", i % PGSIZE, *testptr);
+        if(testptr->slab_idx != (i % PGSIZE)) {
+            cprintf("assert : write %x & read %x\n", i % PGSIZE, testptr->slab_idx);
             goto fail;
         }
         
@@ -348,6 +411,7 @@ void slab_test(void){
         //free_slab_obj(testptr);
     }
     print_slab(slab_idx);
+    print_hash();
 
     for(uint i = test_unit; i < (3 * test_unit); i++){
       free_slab_obj(test_mem[i]);
@@ -356,7 +420,9 @@ void slab_test(void){
     for(uint i = 0; i < (test_unit); i++){
       free_slab_obj(test_mem[i]);
     }
+
     print_slab(slab_idx);
+    print_hash();
 
     for(uint i=0; i < (3 * test_unit); i++){
         TEST_TYPE* testptr = (TEST_TYPE*)kmalloc(sizeof(TEST_TYPE));
@@ -367,10 +433,10 @@ void slab_test(void){
         }
 
         test_mem[i] = testptr;
-        *testptr = i % PGSIZE;
+        testptr->slab_idx = i % PGSIZE;
 
-        if(*testptr != (i % PGSIZE)) {
-            cprintf("assert : write %x & read %x\n", i % PGSIZE, *testptr);
+        if(testptr->slab_idx != (i % PGSIZE)) {
+            cprintf("assert : write %x & read %x\n", i % PGSIZE, testptr->slab_idx);
             goto fail;
         }
         
@@ -379,7 +445,7 @@ void slab_test(void){
     }
 
     for(uint i=0; i < (3 * test_unit); i++){
-        if(*(test_mem[i]) != (i % PGSIZE)) {
+        if(test_mem[i]->slab_idx != (i % PGSIZE)) {
             cprintf("assert : write %x & read %x\n", i % PGSIZE, *(test_mem[i]));
             goto fail;
         }
@@ -387,7 +453,9 @@ void slab_test(void){
         //cprintf("testptr : %x\n", testptr);
         //free_slab_obj(testptr);
     }
+
     print_slab(slab_idx);
+    print_hash();
 
     cprintf("test success!\n");
 
--- slab.h
+++ slab.h
@@ -5,6 +5,7 @@
 
 #define SLAB_NUM 10
 #define MAX_FREE_PAGE 2
+#define HASH_SIZE 20
 
 struct page{
   struct page* next;
@@ -16,9 +17,26 @@ struct page{
   uint* free_list_tail;
 };
 
-struct page_free{
-  struct page* cur_page;
+struct slab_cache{
+  uint obj_sz;
+  uint free_page_num;
+
+  struct page static_page;
+
+  struct page* cur_slab_page;
+  struct page* page_tail;
+};
+
+struct hash_node{
   uint slab_idx;
+  struct page* cur_page;
+  struct hash_node* next;
+  struct hash_node* prev;
+};
+
+struct hash_ptr{
+  struct hash_node* head;
+  struct hash_node* tail;
 };
 
 enum INSERT_FLAG{
@@ -28,16 +46,26 @@ enum INSERT_FLAG{
 
 void make_slab_obj(struct page* cur_page, uint obj_sz);
 struct page alloc_new_slab_page(uint slab_idx);
-struct page_free find_target_page(uint page_addr);
+struct hash_node* find_target_page(uint hash_idx, uint page_addr);
+uint chech_slab_obj(uint obj_addr, uint obj_sz);
 void free_slab_obj(void* slab_obj);
 void free_slab_page(uint slab_idx, struct page* slab_page);
+
+uint calc_hash_idx(uint page_addr);
+void insert_hash_node(uint slab_idx, struct page* slab_page);
+void return_hash_node(uint hash_idx, struct hash_node* tar_node);
+
 void insert_slab_page(uint flag, uint slab_idx, struct page* slab_page);
 void return_slab_page(uint slab_idx, struct page* slab_page);
 void push_free_list(struct page* cur_page, void* slab_obj);
 void* pop_free_list(uint slab_idx);
+
 void slab_init(void);
 void slab_test(void);
+
 void print_slab(uint slab_idx);
+void print_hash(void);
+
 uint calc_slab_idx(uint N);
 void load_next_slab_page(uint slab_idx);
 struct page* alloc_new_page_addr(uint page_size);
